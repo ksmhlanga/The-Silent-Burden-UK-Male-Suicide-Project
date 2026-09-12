b1a6fa9f9dfde8cc4d1193f0fa0130e0d4be5a98
Kudzanayi Shepherd Mhlanga <32449851+ksmhlanga@users.noreply.github.com>
2026-09-12 19:56:49 +0100

    Delete _dev/push_to_github.bat

--- _dev/push_to_github.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-echo Setting up git remote and pushing to GitHub...
-cd /d "C:\Users\Cudzie\Documents\Projects\The Silent Burden - UK Male Suicide Project"
-git remote add origin https://github.com/ksmhlanga/UK_Silent_Crisis_Project.git
-git push -u origin main --force
-echo Done!
-pause
